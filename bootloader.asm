29b6220cd1b3491439f4e7844a0390448e9f439e
moneybagsmahoney <79066541+WheeledCord@users.noreply.github.com>
2025-03-27 14:40:38 +1300

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -1,41 +1,38 @@
-;bootloader.asm multiboot compliant bootloader and interrupt stubs
-section .text
-    align 4
-    dd 0x1BADB002           ;multiboot magic number
-    dd 0x00                 ;flags
-    dd -(0x1BADB002 + 0x00)  ;checksum
-
-global start
-extern k_main
-
-;declare external c interrupt handlers
-extern timer_handler
-extern keyboard_handler
-
+BITS 32
+SECTION .text
+align 4
+dd 0x1BADB002
+dd 0x00
+dd -(0x1BADB002+0x00)
+GLOBAL start
+GLOBAL load_idt
+GLOBAL inb
+GLOBAL outb
+GLOBAL keyboard_interrupt_wrapper
+EXTERN kernel_main
+EXTERN keyboard_handler
 start:
-    cli             ;disable interrupts
-    call k_main     ;call the kernel main function
-    hlt             ;halt if k_main ever returns
-
-;idt loading routine
-global idt_load
-idt_load:
-    mov eax, [esp+4]  ;get pointer to idt_ptr structure
-    lidt [eax]        ;load idt register
+    cli
+    call kernel_main
+    jmp end
+load_idt:
+    mov eax, [esp+4]
+    lidt [eax]
     ret
-
-;timer interrupt stub
-global timer_handler_stub
-timer_handler_stub:
-    pusha                  ;save registers
-    call timer_handler     ;call c timer handler
-    popa                   ;restore registers
-    iret                   ;return from interrupt
-
-;keyboard interrupt stub
-global keyboard_handler_stub
-keyboard_handler_stub:
-    pusha                   ;save registers
-    call keyboard_handler   ;call c keyboard handler
-    popa                    ;restore registers
-    iret                    ;return from interrupt
+inb:
+    mov dx, [esp+4]
+    in al, dx
+    ret
+outb:
+    mov dx, [esp+4]
+    mov al, [esp+8]
+    out dx, al
+    ret
+keyboard_interrupt_wrapper:
+    pusha
+    call keyboard_handler
+    popa
+    iret
+end:
+    hlt
+    jmp end
